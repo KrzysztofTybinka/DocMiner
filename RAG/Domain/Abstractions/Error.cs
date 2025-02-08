3e609fb1a94949cd6060a534b11f30b490d66b52
Krzysztof Tybinka <ktybinka@interia.pl>
2025-02-08 19:05:13 +0100

    Refactor init, domain added, error and result class added to a domain

---
+++ RAG/Domain/Abstractions/Error.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public record Error
+    {
+        public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+
+        private Error(string code, string description, ErrorType errorType)
+        {
+            Code = code;
+            Description = description;
+            ErrorType = errorType;
+        }
+
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public ErrorType ErrorType { get; set; }
+
+        public static Error Failure(string code, string description) =>
+            new(code, description, ErrorType.Failure);
+
+        public static Error Validation(string code, string description) =>
+            new(code, description, ErrorType.Validation);
+
+        public static Error NotFound(string code, string description) =>
+            new(code, description, ErrorType.NotFound);
+
+        public static Error Conflict(string code, string description) =>
+            new(code, description, ErrorType.Conflict);
+    }
+
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validation = 1,
+        NotFound = 2,
+        Conflict = 3
+    }
+}
---
+++ RAG/Domain/ProcessedDocument/ProcessedDocument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ProcessedDocument
+{
+    public class ProcessedDocument
+    {
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        private ProcessedDocument(string name, string content) 
+        {
+            Name = name;
+            Content = content;
+        }
+
+        public ProcessedDocument Create(string name, string content)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+
+            }
+            if (string.IsNullOrEmpty(content))
+            {
+
+            }
+
+            return new ProcessedDocument(name, content);
+        }
+    }
+}
